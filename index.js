768211cbc80025ddfd61bd56249a26a4414cb3d2
Rachel <rachelvweb@gmail.com>
2020-01-07 16:36:57 -0500

    add index file, copy in account route logic, add home template file

---
+++ index.js
@@ -0,0 +1,112 @@
+// DEPENDENCIES 
+
+var express = require("express");
+var app = express();
+
+var bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    flash = require("connect-flash"),
+    sanitizer = require("express-sanitizer");
+// express-session is required below 
+
+// MODELS
+// var User = require("./models/user"),
+//     Deck = require("./models/deck"),
+// 	Card = require("./models/card"),
+// 	Wish = require("./models/wish");
+
+//var middleware = require("./middleware");
+
+
+//var seedDB = require("./seeds");
+//seedDB();
+
+// require routes files
+// var mtgRoutes = require("./routes/mtg"),
+//     wishlistRoutes = require("./routes/wishlist");
+
+
+const PORT = process.env.PORT || 3000
+
+//mongoose.connect("mongodb://localhost/juice_app");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public")); // for a shortcut to the public folder
+app.use(methodOverride("_method")); // for PUT and DELETE
+app.use(flash());
+app.use(sanitizer());
+
+
+app.use(require("express-session")({
+    secret: "Freesh Ava Cadoo",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+
+//middleware used on every route call 
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
+app.get("/", function (req, res) {
+    //show user's decks
+    res.render("home", {});
+});
+
+app.get("/register", function (req, res) {
+    var username = req.query.username;
+    res.render("register", {username: username});
+});
+
+app.post("/register", function (req, res) {
+    if (req.body.password === req.body.password2) {
+        var newUser = new User({ username: req.body.username });
+        User.register(newUser, req.body.password, function (err, user) {
+            if (err) {
+                req.flash("error", err.message);
+                res.redirect("/register");
+            }
+            passport.authenticate("local")(req, res, function () {
+                req.flash("success", "Account created! Welcome!");
+                res.redirect("/");
+            });
+        });
+    } else {
+        req.flash("error", "Passwords don't match");
+        var username = encodeURIComponent(req.username);
+        res.redirect("/register?username=" + username);
+    }
+});
+
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/",
+        failureRedirect: "/",
+        failureFlash: true,
+        successFlash: "Login successful"
+    }), function (req, res) {}
+);
+
+app.get("/logout", function (req, res) {
+    req.logout();
+    req.flash("success", "You have been successfully signed out.");
+    res.redirect("/");
+});
+
+// app.use routes files
+// app.use(mtgRoutes);
+// app.use(wishlistRoutes);
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
